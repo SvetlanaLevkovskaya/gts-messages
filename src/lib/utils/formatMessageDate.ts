323deff04b171f49774ec4eb9ad173520659e92c
levkovskayase@gmail.com <levkovskayase@gmail.com>
2024-05-08 11:54:51 +0300

    fix: корректировка formatMessageDate

--- src/lib/utils/formatMessageDate.ts
+++ src/lib/utils/formatMessageDate.ts
@@ -1,11 +1,12 @@
+const formatNumber = (num: number) => num.toString().padStart(2, '0');
+
 export function formatMessageDate(dateString?: string): string {
   const dateToFormat = dateString ? new Date(dateString) : new Date();
-  return dateToFormat.toLocaleString('ru-RU', {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit',
-  });
+  const year = dateToFormat.getFullYear().toString().slice(-2);
+  const month = formatNumber(dateToFormat.getMonth() + 1);
+  const day = formatNumber(dateToFormat.getDate());
+  const hours = formatNumber(dateToFormat.getHours());
+  const minutes = formatNumber(dateToFormat.getMinutes());
+  const seconds = formatNumber(dateToFormat.getSeconds());
+  return `${day}.${month}.${year} ${hours}:${minutes}:${seconds}`;
 }
